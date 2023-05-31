a9f2f8d7f8908cc13d0b6c581291ec1a5b37236f
Filimon Kiros <filimona.kiros@gmail.com>
2023-05-31 16:19:47 -0400

    test commit on new brnach

--- react-app/src/App.test.js
+++ react-app/src/App.test.js
@@ -1,6 +1,6 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('empty test', () => {
-  expect(true).toBeTruthy();
+test("empty test", () => {
+  expect(false).toBeTruthy();
 });
