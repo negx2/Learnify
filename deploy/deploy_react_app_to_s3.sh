30fd04857c659b20ca7813b0879659c1f67ca341
Filimon Kiros <filimona.kiros@gmail.com>
2023-06-05 17:25:12 -0400

    add aws build for s3

--- deploy/deploy_react_app_to_s3.sh
+++ deploy/deploy_react_app_to_s3.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-bucketName=$(cat ../configs/aws_resources.json | grep -o '"s3_webiste_bucket_name": *"[^"]*"' | cut -d '"' -f 4)
+bucketName=$(cat ../configs/aws_resources.json | grep -o '"s3_webiste_bucket_name": *"[^"]*"' | cut -d '"' -f 4)TrFMX/Caq6H0Y8z9a0Qd4zuXBSSXt5tHeZxyI8yW
 appDirectory="../react-app"
 
 cd "$appDirectory" || exit
 
 npm test -- --watchAll=false --passWithNoTests || exit 1
-npm run build --prefix "$appDirectory" || exit 1
+npm run AWSbuild --prefix "$appDirectory" || exit 1
 
-aws s3 sync "$appDirectory/build/" "s3://$bucketName" || exit 1
+aws s3 sync "$appDirectory/public/bundle/" "s3://$bucketName" || exit 1
