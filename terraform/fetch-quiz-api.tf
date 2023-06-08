c6dd7d1cb6808c1b97a24ff0eb0feeb476faa9ed
Negatu <nnegatu@gmail.com>
2023-06-08 10:22:28 -0700

    Fetch featured api gateway

--- terraform/fetch-quiz-api.tf
+++ terraform/fetch-quiz-api.tf
@@ -1,8 +1,3 @@
-
-data "aws_dynamodb_table" "quizzes_table" {
-  name = "quizzes"
-}
-
 resource "aws_api_gateway_resource" "fetch_quiz_resource" {
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
   parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
@@ -43,6 +38,10 @@ resource "aws_api_gateway_integration_response" "fetch_quiz_integration_response
   status_code = "200"
   depends_on  = [aws_api_gateway_integration.fetch_quiz_integration]
 
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+
   response_templates = {
     "application/json" = <<EOF
 #set($quizRoot = $input.path('$.Item'))
@@ -60,51 +59,15 @@ resource "aws_api_gateway_method_response" "fetch_quiz_method_response" {
   resource_id = aws_api_gateway_resource.fetch_quiz_resource.id
   http_method = aws_api_gateway_method.fetch_quiz_method.http_method
   status_code = "200"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = false
+  }
   response_models = {
     "application/json" = "Empty"
   }
 }
 
-resource "aws_iam_role" "api_gateway_dynamodb_read_role" {
-  name = "api_gateway_dynamodb_read_role"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "apigateway.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_policy" "api_gateway_dynamodb_read_policy" {
-  name = "api_gateway_dynamodb_read_policy"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": "dynamodb:GetItem",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy_attachment" "api_gateway_dynamodb_read_role_policy_attachment" {
-  role       = aws_iam_role.api_gateway_dynamodb_read_role.name
-  policy_arn = aws_iam_policy.api_gateway_dynamodb_read_policy.arn
-}
 
 resource "aws_api_gateway_deployment" "fetch_quiz_api_deployment" {
   depends_on  = [aws_api_gateway_integration.fetch_quiz_integration]
