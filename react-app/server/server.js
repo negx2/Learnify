8002bdd6fb3dfee493dbfb9dc4e3cfa1b2539314
Negatu <nnegatu@gmail.com>
2023-06-08 10:23:00 -0700

    Fetch featured component

--- react-app/server/server.js
+++ react-app/server/server.js
@@ -5,6 +5,7 @@ app.use(express.static("public"));
 
 app.use("/tx", express.static("public"));
 app.use("/fetch", express.static("public"));
+app.use("/featured", express.static("public"));
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
