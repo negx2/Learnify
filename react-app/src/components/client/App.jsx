101736cd74791e1b21a7a8eea8f389d48455a6ee
Filimon Kiros <filimona.kiros@gmail.com>
2023-06-12 12:40:27 -0400

    featured quiz on landing page

--- react-app/src/components/client/App.jsx
+++ react-app/src/components/client/App.jsx
@@ -1,9 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Transcript from "./Transcript.jsx";
-import { questions } from "../HelperFuncs/sampleQnA.js";
-import QuizPlayer from "./QuizPlayer.jsx";
-import FetchQuiz from "./FetchQuiz.jsx";
 import { useNavigate } from "react-router-dom";
 import FetchFeatured from "./FetchFeatured.jsx";
 
@@ -13,23 +10,15 @@ const App = () => {
     <>
       <div>
         <button className="navBTNs" onClick={() => navigate("/")}>
-          Quiz from Sample
+          Home
         </button>
         <button className="navBTNs" onClick={() => navigate("/tx")}>
           Quiz from Transcript
         </button>
-        <button className="navBTNs" onClick={() => navigate("/fetch")}>
-          Quiz from Category
-        </button>
       </div>
-
-      <br></br>
-      <br></br>
       <Routes>
-        <Route path="/" element={<QuizPlayer questions={questions} />} />
+        <Route path="/" element={<FetchFeatured />} />
         <Route path="/tx" element={<Transcript />} />
-        <Route path="/fetch" element={<FetchQuiz />} />
-        <Route path="/featured" element={<FetchFeatured />} />
       </Routes>
     </>
   );
